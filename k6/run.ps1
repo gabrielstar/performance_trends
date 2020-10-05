97bfc4458b92de017c87d81352937287cef2cd41
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-10-05 15:56:06 +0200

    k6 working with docker on localhost

--- k6/run.ps1
+++ k6/run.ps1
@@ -1,2 +1,7 @@
-cat $PSScriptRoot/script.js | docker run -i loadimpact/k6 run --vus 5 --duration 20s -
+cat $PSScriptRoot/script.js | `
+    docker run -i -v ${PWD}:/data `
+        loadimpact/k6 run `
+            --summary-export=/data/export.json `
+            --vus 5 `
+            --duration 20s -
 #cat script.js | docker run -i loadimpact/k6 run --vus 5 --duration 20s -
