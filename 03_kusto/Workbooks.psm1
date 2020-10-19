240e19a7bfe92824614bf82a77e4736e94f5dc8b
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-10-19 12:50:29 +0200

    Added archive tests for troubleshooting

--- 03_kusto/Workbooks.psm1
+++ 03_kusto/Workbooks.psm1
@@ -1,23 +1,28 @@
 Import-Module $PSScriptRoot\Azure.psm1
 
 Function JmeterCSVResultsToJSON($filePathCSV, $filePathJSON)
-{    try
 {
-    $csv = Get-Content -Path $filePathCSV -ErrorAction Stop
-    $csv = $csv | ConvertFrom-Csv | ConvertTo-Json | Out-File -Encoding UTF8 $filePathJSON
-}catch [System.Management.Automation.ItemNotFoundException] {
-    "IO Error while rading/writing file: {0},{1}" -f $filePathCSV, $filePathJSON
-    "Terminating"
-}
-return
+    try
+    {
+        $csv = Get-Content -Path $filePathCSV -ErrorAction Stop
+        $csv = $csv | ConvertFrom-Csv | ConvertTo-Json | Out-File -Encoding UTF8 $filePathJSON
+    }
+    catch [System.Management.Automation.ItemNotFoundException]
+    {
+        "IO Error while rading/writing file: {0},{1}" -f $filePathCSV, $filePathJSON
+        "Terminating"
+    }
+    return
 
 }
 
-Function LoadProperties($propertiesFilePath){
+Function LoadProperties($propertiesFilePath)
+{
     $properties = ConvertFrom-StringData (Get-Content -Path $propertiesFilePath -raw -ErrorAction Stop)
     return $properties
 }
-Function SendRawDataToLogAnalytics($propertiesFilePath, $filePathJSON){
+Function SendRawDataToLogAnalytics($propertiesFilePath, $filePathJSON)
+{
 
     $properties = LoadProperties -propertiesFilePath $propertiesFilePath
     $body = Get-Content -Path $filePathJSON
@@ -28,20 +33,24 @@ Function SendRawDataToLogAnalytics($propertiesFilePath, $filePathJSON){
     return $statusCode
 }
 
-Function AddColumnToCSV($filePathCSV, $outFilePathCSV, $columnHeader, $columnFieldsValue){
+Function AddColumnToCSV($filePathCSV, $outFilePathCSV, $columnHeader, $columnFieldsValue)
+{
     Write-Host "$columnHeader -> $columnFieldsValue"
     Import-Csv $filePathCSV |
-            Select-Object *,@{Name=$columnHeader;Expression={"$columnFieldsValue"}} |
+            Select-Object *, @{ Name = $columnHeader; Expression = { "$columnFieldsValue" } } |
             Export-Csv "$outFilePathCSV" -NoTypeInformation
 }
 
-Function SendDataToLogAnalytics($propertiesFilePath, $filePathCSV, $filePathJSON){
+Function SendDataToLogAnalytics($propertiesFilePath, $filePathCSV, $filePathJSON)
+{
     $status = 999
     try
     {
         JmeterCSVResultsToJSON -filePathCSV $filePathCSV -filePathJSON $filePathJSON
         $status = SendRawDataToLogAnalytics -propertiesFilePath $propertiesFilePath -filePathJSON $filePathJSON
-    }catch {
+    }
+    catch
+    {
         Write-Host "Unexpected exception"
         Write-Host $_
         Write-Host $_.ScriptStackTrace
